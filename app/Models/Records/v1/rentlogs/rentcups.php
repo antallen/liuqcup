b22dfa982d1a19690e1a3653ee978f1a896d28fa
antallen <antallen@gmail.com>
2021-06-10 21:33:27 +0800

    modify runtcups

--- app/Models/Records/v1/rentlogs/rentcups.php
+++ app/Models/Records/v1/rentlogs/rentcups.php
@@ -110,7 +110,7 @@ class  rentcups extends Model
                                         ->where('storeid',$value->storeid)->get();
                         $totals['rentid'] = "己借杯數量";
                         //$totals['storeid'] = strval($value->storeid);
-                        $totals['storeid'] = strval($storename->storename);
+                        $totals['storeid'] = strval($storename[0]->storename);
                         $totals['datetime'] = $dateTimes;
                         $totals['nums'] = intval($nums);
                         //$tatols['借杯數量'][$dateTimes][strval($value->storeid)] = intval($nums);
