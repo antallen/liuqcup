a0bfa6072e966f2c54cf14712fd77056829e6cdc
antallen <antallen@gmail.com>
2021-06-12 16:32:01 +0800

    Modify rentlog

--- app/Models/Records/v1/rentlogs/rentlog.php
+++ app/Models/Records/v1/rentlogs/rentlog.php
@@ -202,11 +202,11 @@ class  rentlog extends Model
         //查全部
         $result = DB::select('(select a.eventtimes,a.cusphone,c.storename as rentstore,a.nums,a.backtimes,d.storename as backstore
                     from rentlogs as a join stores as c, stores as d
-                    where a.storeid = c.storeid and a.backstoreid = d.storeid order by a.eventtimes desc)
+                    where a.storeid = c.storeid and a.backstoreid = d.storeid)
                     UNION
                     (select a.eventtimes,a.cusphone,c.storename as rentstore,a.nums,a.backtimes,a.backstoreid as backstore
                     from rentlogs as a join stores as c
-                    where a.storeid = c.storeid and a.backstoreid is null order by a.eventtimes desc)');
+                    where a.storeid = c.storeid and a.backstoreid is null) order by a.eventtimes desc');
         return $result;
         }
     }
